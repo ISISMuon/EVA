37679ec3202c9e4fc1dd18b51bd8a6f0af5d7d86
Matea-STFC <tea.bjorklund@stcf.ac.uk>
2025-03-21 15:10:58 +0000

    add worker class to dispatch processes on separate threads

---
+++ src/EVA/util/worker.py
@@ -0,0 +1,65 @@
+"""
+This code has been adapted from https://www.pythonguis.com/tutorials/multithreading-pyqt6-applications-qthreadpool/
+"""
+import sys
+import time
+import traceback
+from PyQt6.QtCore import QRunnable, pyqtSlot, QObject, pyqtSignal
+
+class WorkerSignals(QObject):
+    """Signals from a running worker thread.
+
+    finished
+        No data
+
+    error
+        tuple (exctype, value, traceback.format_exc())
+
+    result
+        object data returned from processing, anything
+    """
+
+    finished = pyqtSignal()
+    cancelled = pyqtSignal()
+    error = pyqtSignal(tuple)
+    result = pyqtSignal(object)
+    progress = pyqtSignal(dict)
+
+class Worker(QRunnable):
+    """Worker thread.
+
+    Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
+
+    :param callback: The function callback to run on this worker thread.
+                     Supplied args and
+                     kwargs will be passed through to the runner.
+    :type callback: function
+    :param args: Arguments to pass to the callback function
+    :param kwargs: Keywords to pass to the callback function
+    """
+
+    def __init__(self, fn, *args, **kwargs):
+        super().__init__()
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = WorkerSignals()
+
+        self.kwargs["progress_callback"] = self.signals.progress
+
+    @pyqtSlot()
+    def run(self):
+        """Initialise the runner function with passed args, kwargs."""
+
+        # Retrieve args/kwargs here; and fire processing using them
+        try:
+            result = self.fn(*self.args, **self.kwargs)
+
+        except Exception:
+            traceback.print_exc()
+            exctype, value = sys.exc_info()[:2]
+            self.signals.error.emit((exctype, value, traceback.format_exc()))
+        else:
+            self.signals.result.emit(result)  # Return the result of the processing
+        finally:
+            self.signals.finished.emit()  # Done
