599f36579567d8ab966fd748c5df267b0a4cd920
chendurshr <142641548+chendurshr@users.noreply.github.com>
2026-03-31 05:01:52 +0100

    port srim classes used for determining a,z from chemical formula

--- src/g4bl/__init__.py
+++ src/g4bl/__init__.py
@@ -1,2 +1,2 @@
 from .g4bl import G4BL
-from .core import InputWriter, Shape, Slab, Cylinder, Sphere
+from .core import InputWriter, Shape, Slab, Cylinder, Sphere, ElementDB, Element, Material
--- src/g4bl/core/__init__.py
+++ src/g4bl/core/__init__.py
@@ -1,2 +1,5 @@
 from .input_writer import InputWriter
 from .shapes import Shape, Slab, Cylinder, Sphere
+from .element import Element
+from .elementdb import ElementDB
+from .material import Material
