5240826cb9dd7cb9f7b414d4115186edba30f30f
chendurshr <142641548+chendurshr@users.noreply.github.com>
2026-04-19 21:04:20 +0100

    Include biriani run data in test cases

--- tests/gui/test_workspace_window.py
+++ tests/gui/test_workspace_window.py
@@ -63,34 +63,31 @@ class TestLoadWorkspaceWindow:
             assert flags["no_files_found"]
         else:
             self.window = WorkspaceWindow(run)
-            self.view = self.window._view
+            self.view = self.window.widget()
             qtbot.addWidget(self.view)
             self.model = self.window._model
             self.window.show()
             qtbot.wait(500)
             assert self.model.run.run_num == run_num
-            if self.run_copy.data_type == "biriani":
-                assert self.run_copy.plot_mode == "Biriani Spectrum"
-            elif self.run_copy.data_type == "nexus":
-                self.view.nexus_plot_display_combo_box.setCurrentText(test_plot_mode)
-                self.view.normalisation_type_combo_box.setCurrentText(
-                    normalisation_to_ui_text[test_normalisation]
-                )
-                self.view.binning_spin_box.setValue(test_binning)
-                qtbot.mouseClick(
-                    self.view.apply_run_settings_button, Qt.MouseButton.LeftButton
-                )
-                qtbot.wait(500)
-                self.run_copy.set_corrections(
-                    plot_mode=test_plot_mode,
-                    normalisation=test_normalisation,
-                    bin_rate=float(test_binning),
-                    prompt_limit=2000,
-                    delayed_limit=20000000,
-                )
-                for i, (run_spectrum, run_copy_spectrum) in enumerate(
-                    zip(self.model.run._raw.values(), self.run_copy._raw.values())
-                ):
-                    assert np.array_equal(run_spectrum.x, run_copy_spectrum.x)
-                    assert np.array_equal(run_spectrum.y, run_copy_spectrum.y)
+            self.view.nexus_plot_display_combo_box.setCurrentText(test_plot_mode)
+            self.view.normalisation_type_combo_box.setCurrentText(
+                normalisation_to_ui_text[test_normalisation]
+            )
+            self.view.binning_spin_box.setValue(test_binning)
+            qtbot.mouseClick(
+                self.view.apply_run_settings_button, Qt.MouseButton.LeftButton
+            )
+            qtbot.wait(500)
+            self.run_copy.set_corrections(
+                plot_mode=test_plot_mode,
+                normalisation=test_normalisation,
+                bin_rate=float(test_binning),
+                prompt_limit=2000,
+                delayed_limit=20000000,
+            )
+            for i, (run_spectrum, run_copy_spectrum) in enumerate(
+                zip(self.model.run._raw.values(), self.run_copy._raw.values())
+            ):
+                assert np.array_equal(run_spectrum.x, run_copy_spectrum.x)
+                assert np.array_equal(run_spectrum.y, run_copy_spectrum.y)
             matplotlib.pyplot.close()
